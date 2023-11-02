6c4f9b5ac2b9584e038c3ce5b3aee22a3732b602
Bobby11th <149469831+Bobby11th@users.noreply.github.com>
2023-11-02 17:34:42 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,12 +1,6 @@
 <?php
-if (isset($_POST['email']) && !empty($_POST['email'])) {
-$subject = "New contact request: $_POST['subject']";
-$message = $_POST['body'];
-$headers = 'From: facebook.com' . "\r\n" .
-           'Reply-To: ' . $_POST['email']. "\r\n" .
-           'X-Mailer: PHP/' . phpversion();
-
-mail('classicvibe766@gmail.com', $subject, $message, $headers);
-
-die('Thank you for your email');
+if($_POST["Message"]) {
+mail("classicvibe766@gmail.com", "Here is the sample subject line",
+$_POST["Insert Your Message"]. "From: jane@janedoe.com");
 }
+?>
